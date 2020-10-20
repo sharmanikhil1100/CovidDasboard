30eb890d4367f390aa9536992d1673e04ac691cc
Nik <sharmanikhil1100@gmail.com>
2020-10-21 00:11:16 +0530

    Post request in JSON done. CORS added.

--- covid/views.py
+++ covid/views.py
@@ -1,21 +1,25 @@
 from django.http import HttpResponse
 from .models import CovidData
+import json
 
 def index(request):
     return HttpResponse("Hello, world. You're at the polls index.")
 
 def get_data(request):
     if request.method=="GET":
+        # x = CovidData.objects.all()
+        # return json.loads(x)
         return HttpResponse("Data!")
     return HttpResponse("Invalid Request", status=400)
 
 def insert(request):
     if request.method=="POST":
+        input_data=json.loads(request.body)        
         covidData=CovidData()
-        covidData.patient_no=1
-        covidData.city="c1"
-        covidData.state="s1"
-        covidData.date="d1"
+        covidData.patient_no=input_data["patient_no"]
+        covidData.city=input_data["city"]
+        covidData.state=input_data["state"]
+        covidData.date=input_data["date"]
         covidData.save()
         return HttpResponse("Data Inserted!")
     return HttpResponse("Invalid Request", status=400)
