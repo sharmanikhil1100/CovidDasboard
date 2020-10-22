2ef9010e8f2e23e805e7723c31f1910fb9ad291b
Nik <sharmanikhil1100@gmail.com>
2020-10-22 11:00:28 +0530

    Added the query for getting data from db in views.py

--- covid/views.py
+++ covid/views.py
@@ -1,15 +1,30 @@
 from django.http import HttpResponse
+from django.shortcuts import render
 from .models import CovidData
 import json
 
+encoding = 'utf-8'
+
 def index(request):
-    return HttpResponse("Hello, world. You're at the polls index.")
+    states = ['gujarat, maharashtra, andhra pradesh']
+    return render(request, 'covid/get-all', {
+        'state': states,
+    }
+    )
 
 def get_data(request):
     if request.method=="GET":
         # x = CovidData.objects.all()
         # return json.loads(x)
-        return HttpResponse("Data!")
+        data = str(request.body, encoding)
+        covidData=CovidData()
+        query_result = []
+
+        for p in CovidData.objects.raw('SELECT * FROM covid_covidData'):
+            query_result.append(p)
+        return render(request, 'home/home.html', {
+            'state_data':query_result
+        })
     return HttpResponse("Invalid Request", status=400)
 
 def insert(request):
