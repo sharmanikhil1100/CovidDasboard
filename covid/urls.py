19bd6a96c9590f17876a2470a33ce39b25b4874d
Nik <sharmanikhil1100@gmail.com>
2020-10-22 19:06:33 +0530

    Retrieve and display data in HTML

--- covid/urls.py
+++ covid/urls.py
@@ -3,7 +3,8 @@ from django.views.decorators.csrf import csrf_exempt
 from . import views
 
 urlpatterns = [
-    path('', views.index),
+    # path('', views.index),
+    path('', views.home),
     path('get-all', views.get_data),
     path('insert', csrf_exempt(views.insert)),
 ]
--- covid/views.py
+++ covid/views.py
@@ -1,6 +1,7 @@
 from django.http import HttpResponse
 from django.shortcuts import render
 from .models import CovidData
+from django.core.serializers import serialize
 import json
 
 encoding = 'utf-8'
@@ -12,31 +13,29 @@ def index(request):
     }
     )
 
-def get_data(request):
-    if request.method=="GET":
-        # x = CovidData.objects.all()
-        # return json.loads(x)
-        data = str(request.body, encoding)
-        covidData=CovidData()
-        query_result = []
+def home(request):
+    return render(request, 'home/home.html', {"data_rows":json.loads(get_all_data())} )
 
-        for p in CovidData.objects.raw('SELECT * FROM covid_covidData'):
-            query_result.append(p)
-        print(query_result)
-        return render(request, 'home/home.html', {
-            'state_data':query_result
-        })
+def get_data(request):
+    if request.method=="GET":                
+        # for entry in query_result:
+        #     print(entry['pk'])        
+        # return HttpResponse(get_all_data())
+        return render(request, 'home/home.html', {"data_rows":json.loads(get_all_data())} )
     return HttpResponse("Invalid Request", status=400)
+
+def get_all_data():
+    return serialize('json', CovidData.objects.all())
 
 def insert(request):
     if request.method=="POST":
         print(request.POST)
-        input_data=request.POST # Showing error
+        input_data=request.POST
         covidData=CovidData()
         covidData.patient_no=input_data["patient_no"]
         covidData.city=input_data["city"]
         covidData.state=input_data["state"]
         covidData.date=input_data["date"]
         covidData.save()
-        return HttpResponse("Data Inserted!")
+        return render(request, 'home/home.html', {"data_rows":json.loads(get_all_data())} )
     return HttpResponse("Invalid Request", status=400)
